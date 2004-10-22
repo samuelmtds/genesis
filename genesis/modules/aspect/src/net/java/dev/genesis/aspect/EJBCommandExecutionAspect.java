78de5e21cb8cff4f71526c05286787685ffab72f
allanjones <allan.jones@gmail.com>
2004-10-22 19:51:46 +0000

    Issue #45. Fixed. Home is not cached statically because the EJBCommandInvocationAspect is deployed perJVM. RemoteException is never wrapped in InvocationTargetException, so we can catch RemoteException and sets the EJB instance to null.
    git-svn-id: https://svn.java.net/svn/genesis~svn/trunk@100 c226d7c4-892a-e4a7-9418-86fe0cfb12d3

--- genesis/modules/aspect/src/net/java/dev/genesis/aspect/EJBCommandExecutionAspect.java
+++ genesis/modules/aspect/src/net/java/dev/genesis/aspect/EJBCommandExecutionAspect.java
@@ -19,10 +19,15 @@
 package net.java.dev.genesis.aspect;
 
 import java.lang.reflect.InvocationTargetException;
+import java.rmi.RemoteException;
+
 import javax.naming.InitialContext;
 import javax.rmi.PortableRemoteObject;
 import net.java.dev.genesis.ejb.CommandExecutor;
 import net.java.dev.genesis.ejb.CommandExecutorHome;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.codehaus.aspectwerkz.CrossCuttingInfo;
 import org.codehaus.aspectwerkz.joinpoint.JoinPoint;
 import org.codehaus.aspectwerkz.joinpoint.MethodRtti;
@@ -32,57 +37,83 @@ import org.codehaus.aspectwerkz.joinpoint.impl.MethodRttiImpl;
  * @Aspect perJVM
  */
 public class EJBCommandExecutionAspect extends CommandInvocationAspect {
-    public EJBCommandExecutionAspect(CrossCuttingInfo ccInfo) {
-        super(ccInfo);
-    }
+   private static Log log = LogFactory.getLog(EJBCommandExecutionAspect.class);
 
-    private CommandExecutor getCommandExecutor() {
-        try{
-            final CommandExecutorHome home = (CommandExecutorHome) PortableRemoteObject.narrow(new InitialContext().lookup(ccInfo
-                    .getParameter("jndiName")), CommandExecutorHome.class);
-            return home.create();
-        }
-        catch(final Exception e){
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-    }
+   private CommandExecutorHome home;
+   private CommandExecutor session;
 
-    /**
-     * 
-     * @param jp
-     *            the join point
-     * @return the resulting object
-     * @throws Throwable
-     * 
-     * @Around ejbCommandExecution
-     */
-    public Object commandExecution(final JoinPoint jp) throws Throwable {
-        
-        final MethodRtti rtti = (MethodRtti)jp.getRtti();
-        final CommandResolver obj = (CommandResolver)jp.getTarget();
-        try {
-            final Class[] classes = rtti.getParameterTypes();
-            final String[] classNames = new String[classes.length];
+   public EJBCommandExecutionAspect(CrossCuttingInfo ccInfo) {
+      super(ccInfo);
+   }
 
-            for(int i = 0; i < classes.length; i++){
-                classNames[i] = classes[i].getName();
-            }
+   private CommandExecutorHome getHome() throws Exception {
+      if (home != null) {
+         return home;
+      }
+      home = (CommandExecutorHome)PortableRemoteObject.narrow(
+            new InitialContext().lookup(ccInfo.getParameter("jndiName")),
+            CommandExecutorHome.class);
+      return home;
+   }
 
-            final CommandExecutor commandExecutor = getCommandExecutor();
+   private CommandExecutor getCommandExecutor() {
+      if (session != null) {
+         return session;
+      }
+      try {
+         session = getHome().create();
+         return session;
+      } catch (final Exception e) {
+         home = null;
+         log.error(e.getMessage(), e);
+         throw new RuntimeException(e);
+      }
+   }
 
-            try {
-                final String methodName = ((MethodRttiImpl)rtti).getMethodTuple().getWrapperMethod().getName();
-                final Object[] parameterValues = rtti.getParameterValues();
-                if(obj.isTransaction(rtti.getMethod())){
-                    return commandExecutor.executeTransaction(obj, methodName, classNames, parameterValues);
-                }
-                return commandExecutor.executeQuery(obj, methodName, classNames, parameterValues);
-            } finally {
-                commandExecutor.remove();
-            }
-        } catch(final InvocationTargetException ite) {
-            throw ite.getTargetException();
-        }
-    }
+   /**
+    * 
+    * @param jp
+    *            the join point
+    * @return the resulting object
+    * @throws Throwable
+    * 
+    * @Around ejbCommandExecution
+    */
+   public Object commandExecution(final JoinPoint jp) throws Throwable {
+
+      final MethodRtti rtti = (MethodRtti)jp.getRtti();
+      final CommandResolver obj = (CommandResolver)jp.getTarget();
+      final Class[] classes = rtti.getParameterTypes();
+      final String[] classNames = new String[classes.length];
+      final String methodName = ((MethodRttiImpl)rtti).getMethodTuple()
+            .getWrapperMethod().getName();
+      final Object[] parameterValues = rtti.getParameterValues();
+
+      for (int i = 0; i < classes.length; i++) {
+         classNames[i] = classes[i].getName();
+      }
+
+      CommandExecutor commandExecutor = null;
+
+      try {
+         commandExecutor = getCommandExecutor();
+         if (obj.isTransaction(rtti.getMethod())) {
+            return commandExecutor.executeTransaction(obj, methodName,
+                  classNames, parameterValues);
+         }
+         return commandExecutor.executeQuery(obj, methodName, classNames,
+               parameterValues);
+      } catch (final RemoteException re) {
+         log.error("RemoteException occured", re);
+         session = null;
+         throw re;
+      } catch (final InvocationTargetException ite) {
+         throw ite.getTargetException();
+      } finally {
+         if (commandExecutor != null) {
+            commandExecutor.remove();
+         }
+      }
+
+   }
 }
