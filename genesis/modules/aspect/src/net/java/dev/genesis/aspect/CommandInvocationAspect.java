6f3a22402003170fc61136ddf5ec067e3fb62b52
mister__m <misterm@gmail.com>
2004-11-13 19:41:33 +0000

    CommandResolverImpl is also Serializable now.
    git-svn-id: https://svn.java.net/svn/genesis~svn/trunk@207 c226d7c4-892a-e4a7-9418-86fe0cfb12d3

--- genesis/modules/aspect/src/net/java/dev/genesis/aspect/CommandInvocationAspect.java
+++ genesis/modules/aspect/src/net/java/dev/genesis/aspect/CommandInvocationAspect.java
@@ -1,23 +1,24 @@
 /*
  * The Genesis Project
  * Copyright (C) 2004  Summa Technologies do Brasil Ltda.
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 package net.java.dev.genesis.aspect;
 
+import java.io.Serializable;
 import java.lang.reflect.Method;
 import net.java.dev.genesis.command.Query;
 import net.java.dev.genesis.command.Transaction;
@@ -34,7 +35,7 @@ public class CommandInvocationAspect {
    /**
     * @Introduce commandResolverIntroduction deployment-model=perJVM
     */
-   public static class CommandResolverImpl implements CommandResolver {
+   public static class CommandResolverImpl implements CommandResolver, Serializable {
       public boolean isRemotable(Method m) {
          return Annotations.getAnnotation("Remotable", m) != null
                || Query.class.isAssignableFrom(m.getDeclaringClass())
@@ -50,7 +51,7 @@ public class CommandInvocationAspect {
        * <li>The method must contain at least one <code>Transactional</code>
        * annotation.</li>
        * </ul>
-       * 
+       *
        * @param m
        *            The proxy method
        */
