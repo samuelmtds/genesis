f0c68ade6237e51a473189aef1776511cbd5e9ea
mister__m <misterm@gmail.com>
2004-11-24 02:31:55 +0000

    Fix for issue # 77.
    git-svn-id: https://svn.java.net/svn/genesis~svn/trunk@294 c226d7c4-892a-e4a7-9418-86fe0cfb12d3

--- genesis/modules/aspect/src/net/java/dev/genesis/aspect/TimeoutAspect.java
+++ genesis/modules/aspect/src/net/java/dev/genesis/aspect/TimeoutAspect.java
@@ -1,17 +1,17 @@
 /*
  * The Genesis Project
  * Copyright (C) 2004  Summa Technologies do Brasil Ltda.
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -19,6 +19,7 @@
 package net.java.dev.genesis.aspect;
 
 import net.java.dev.genesis.exception.TimeoutException;
+import org.apache.commons.logging.Log;
 
 import org.apache.commons.logging.LogFactory;
 import org.codehaus.aspectwerkz.CrossCuttingInfo;
@@ -28,48 +29,139 @@ import org.codehaus.aspectwerkz.joinpoint.JoinPoint;
  * @Aspect perThread
  */
 public class TimeoutAspect {
-    private final long timeout;
-    
-    private Throwable t;
-    private Object ret;
+   private static final Log log = LogFactory.getLog(TimeoutAspect.class);
 
-    public TimeoutAspect(final CrossCuttingInfo ccInfo) {
-        this.timeout = Long.parseLong(ccInfo.getParameter("timeout"));
-    }
-    
-    /**
-     * @Around timeout
-     * 
-     */
-    public Object timeoutAdvice(final JoinPoint jp) throws Throwable {
-        t = null;
-        ret = null;
+   private static class WorkerThread extends Thread {
+      private Throwable throwable;
+      private Object returnValue;
+      private JoinPoint jp;
+      private Object lock = new Object();
+      private boolean started;
+      private boolean running;
 
-        final Thread thread = new Thread() {
-            public void run() {
-                try {
-                    ret = jp.proceed();
-                } catch (final Throwable th) {
-                    t = th;
-                }
+      public WorkerThread() {
+         setDaemon(true);
+      }
+
+      public void execute(JoinPoint jp, long timeout) {
+         this.jp = jp;
+         returnValue = null;
+         throwable = null;
+         running = true;
+
+         if (!started) {
+            started = true;
+            start();
+         } else {
+            synchronized (lock) {
+               lock.notifyAll();
             }
-        };
-        thread.start();
-        thread.join(timeout);
+         }
 
-        if (thread.isAlive()) {
+         synchronized (lock) {
             try {
-                thread.interrupt();
-            } catch (Exception e) {
-                LogFactory.getLog(getClass()).error(e);
+               lock.wait(timeout);
+            } catch (InterruptedException ie) {
+               log.info(ie);
+            }
+         }
+      }
+
+      public void run() {
+         do {
+            try {
+               returnValue = jp.proceed();
+            } catch (final Throwable t) {
+               throwable = t;
             }
 
-            throw new TimeoutException("Execution took more than " + timeout + " ms");
-        }
+            running = false;
+            jp = null;
 
-        if (t != null) {
-            throw t;
-        }
-        return ret;
-    }
+            synchronized (lock) {
+               lock.notifyAll();
+
+               try {
+                  lock.wait();
+               } catch (InterruptedException ie) {
+                  log.info(ie);
+               }
+            }
+         } while (!isInterrupted());
+      }
+
+      public Object getReturnValue() {
+         return returnValue;
+      }
+
+      public Throwable getThrowable() {
+         return throwable;
+      }
+
+      public boolean isRunning() {
+         return running;
+      }
+
+      public void cleanUp() {
+         returnValue = null;
+         throwable = null;
+      }
+   }
+
+   private final long timeout;
+   private final boolean keepThreadInstance;
+   private WorkerThread thread;
+   
+   public TimeoutAspect(final CrossCuttingInfo ccInfo) {
+      this.timeout = Long.parseLong(ccInfo.getParameter("timeout"));
+      this.keepThreadInstance = "true".equals(
+            ccInfo.getParameter("keepThreadInstance"));
+   }
+   
+   /**
+    * @Around timeout
+    */
+   public Object timeoutAdvice(final JoinPoint jp) throws Throwable {
+      if (thread == null) {
+         thread = new WorkerThread();
+
+         if (keepThreadInstance && log.isDebugEnabled()) {
+            log.debug("New thread created: " + thread);
+         }
+      } else {
+         if (keepThreadInstance && log.isDebugEnabled()) {
+            log.debug("Reusing thread: " + thread);
+         }
+      }
+
+      thread.execute(jp, timeout);
+
+      if (thread.isRunning()) {
+         try {
+            thread.interrupt();
+         } catch (Throwable t) {
+            log.error(t);
+         }
+
+         thread = null;
+
+         throw new TimeoutException("Execution took more than " + timeout + 
+               " ms");
+      }
+ 
+      final Throwable throwable = thread.getThrowable();
+      final Object returnValue = thread.getReturnValue();
+
+      if (keepThreadInstance) {
+         thread.cleanUp();
+      } else {
+         thread = null;
+      }
+
+      if (throwable != null) {
+         throw throwable;
+      }
+
+      return returnValue;
+   }
 }
