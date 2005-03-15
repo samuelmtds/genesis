18171c3be180d9e41188fb11b8610bbfaf97cd2e
mister__m <misterm@gmail.com>
2005-03-15 21:17:41 +0000

    Test moved.
    git-svn-id: https://svn.java.net/svn/genesis~svn/branches/GENESIS_0_2_X_BRANCH@594 c226d7c4-892a-e4a7-9418-86fe0cfb12d3

--- genesis/modules/client/test-src/net/java/dev/genesis/commons/beanutils/converters/BigDecimalConverterTest.java
+++ genesis/modules/client/test-src/net/java/dev/genesis/commons/beanutils/converters/BigDecimalConverterTest.java
@@ -1,36 +1,32 @@
 /*
  * The Genesis Project
- * Copyright (C) 2004  Summa Technologies do Brasil Ltda.
- * 
+ * Copyright (C) 2005  Summa Technologies do Brasil Ltda.
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-package net.java.dev.genesis.tests.converters;
+package net.java.dev.genesis.commons.beanutils.converters;
 
 import java.math.BigDecimal;
 
-import net.java.dev.genesis.tests.TestCase;
+import net.java.dev.genesis.GenesisTestCase;
 
-import org.apache.commons.beanutils.ConvertUtils;
-import org.apache.commons.beanutils.Converter;
-
-public class BigDecimalConverterTest extends TestCase {
-
+public class BigDecimalConverterTest extends GenesisTestCase {
    public void testBigDecimalConverter() {
       BigDecimal bigDecimal = new BigDecimal("5.2");
 
-      final Converter converter = ConvertUtils.lookup(BigDecimal.class);
+      final BigDecimalConverter converter = new BigDecimalConverter();
 
       assertTrue(new BigDecimal("0.0").compareTo((BigDecimal) converter
             .convert(BigDecimal.class, "0,0")) == 0);
