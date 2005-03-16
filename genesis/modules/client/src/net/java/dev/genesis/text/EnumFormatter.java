a124b03da10f5a035f9098d13b8644b42fe949ae
mister__m <misterm@gmail.com>
2005-03-16 03:54:21 +0000

    Modifications related to tests
    git-svn-id: https://svn.java.net/svn/genesis~svn/branches/GENESIS_0_2_X_BRANCH@598 c226d7c4-892a-e4a7-9418-86fe0cfb12d3

--- genesis/modules/client/src/net/java/dev/genesis/text/EnumFormatter.java
+++ genesis/modules/client/src/net/java/dev/genesis/text/EnumFormatter.java
@@ -1,6 +1,6 @@
 /*
  * The Genesis Project
- * Copyright (C) 2004  Summa Technologies do Brasil Ltda.
+ * Copyright (C) 2004-2005  Summa Technologies do Brasil Ltda.
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -19,13 +19,16 @@
 package net.java.dev.genesis.text;
 
 import java.util.MissingResourceException;
-
 import net.java.dev.genesis.ui.UIUtils;
 import net.java.dev.reusablecomponents.lang.Enum;
 import org.apache.commons.logging.LogFactory;
 
 public class EnumFormatter implements Formatter {
    public String format(final Object o) {
+      if (o == null) {
+         return "";
+      }
+
       final Enum en = (Enum)o;
 
       String className = en.getBaseClass().getName();
