4fffa9cb4b650026c00ce64f7d9502d8dcd06788
mister__m <misterm@gmail.com>
2005-03-17 02:07:57 +0000

    Fix for issue # 217.
    git-svn-id: https://svn.java.net/svn/genesis~svn/branches/GENESIS_0_2_X_BRANCH@606 c226d7c4-892a-e4a7-9418-86fe0cfb12d3

---
+++ genesis/modules/client/src/net/java/dev/genesis/commons/beanutils/converters/BooleanConverter.java
@@ -0,0 +1,83 @@
+/*
+ * The Genesis Project
+ * Copyright (C) 2005  Summa Technologies do Brasil Ltda.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package net.java.dev.genesis.commons.beanutils.converters;
+
+import org.apache.commons.beanutils.ConversionException;
+import org.apache.commons.beanutils.Converter;
+
+public class BooleanConverter implements Converter {
+   public static final String TRUE = "true";
+   public static final String FALSE = "false";
+
+   private final Object trueValue;
+   private final Object falseValue;
+   private final boolean returnDefaultValue;
+   private final boolean nullAsDefaultValue;
+   private final Boolean defaultValue;
+
+   public BooleanConverter() {
+      this(TRUE, FALSE, true, false, Boolean.FALSE);
+   }
+
+   public BooleanConverter(Object trueValue, Object falseValue) {
+      this(trueValue, falseValue, true, false, Boolean.FALSE);
+   }
+
+   public BooleanConverter(Object trueValue, Object falseValue, 
+         Boolean defaultValue) {
+      this(trueValue, falseValue, true, false, defaultValue);
+   }
+
+   public BooleanConverter(Object trueValue, Object falseValue, 
+         boolean returnDefaultValue) {
+      this(trueValue, falseValue, returnDefaultValue, false,
+            returnDefaultValue ?  Boolean.FALSE : null);
+   }
+
+   public BooleanConverter(Object trueValue, Object falseValue, 
+         boolean returnDefaultValue, boolean nullAsDefaultValue, 
+         Boolean defaultValue) {
+      this.trueValue = trueValue;
+      this.falseValue = falseValue;
+      this.returnDefaultValue = returnDefaultValue;
+      this.nullAsDefaultValue = nullAsDefaultValue;
+      this.defaultValue = defaultValue;
+   }
+
+   public Object convert(Class clazz, Object obj) {
+      if (obj == null) {
+         return nullAsDefaultValue ? defaultValue : null;
+      }
+
+      if (obj.getClass().equals(Boolean.class)) {
+         return obj;
+      }
+
+      if (trueValue.equals(obj)) {
+         return Boolean.TRUE;
+      } else if (falseValue.equals(obj)) {
+         return Boolean.FALSE;
+      } else if (returnDefaultValue) {
+         return defaultValue;
+      } else {
+         throw new ConversionException(obj + " cannot be converted to " +
+               clazz.getName() + "; its type is " + obj.getClass().getName());
+      }
+   }
+}
