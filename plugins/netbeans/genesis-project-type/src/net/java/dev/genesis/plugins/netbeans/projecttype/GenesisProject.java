7b80869ecbf588ee118b62d3d2fb07907e5374c0
mister__m <misterm@gmail.com>
2006-01-10 19:14:37 +0000

    Project icon is now shown in Open Recent Projects and copyright notices added (issue # 314, still in progress)
    git-svn-id: https://svn.java.net/svn/genesis~svn/trunk@920 c226d7c4-892a-e4a7-9418-86fe0cfb12d3

--- plugins/netbeans/genesis-project-type/src/net/java/dev/genesis/plugins/netbeans/projecttype/GenesisProject.java
+++ plugins/netbeans/genesis-project-type/src/net/java/dev/genesis/plugins/netbeans/projecttype/GenesisProject.java
@@ -1,8 +1,27 @@
+/*
+ * The Genesis Project
+ * Copyright (C) 2006  Summa Technologies do Brasil Ltda.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package net.java.dev.genesis.plugins.netbeans.projecttype;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import net.java.dev.genesis.plugins.netbeans.projecttype.ui.GenesisLogicalViewProvider;
 import org.netbeans.api.project.Project;
 import org.netbeans.api.project.ProjectInformation;
@@ -11,6 +30,7 @@ import org.netbeans.spi.project.support.ant.AntProjectHelper;
 import org.netbeans.spi.project.support.ant.PropertyEvaluator;
 import org.openide.filesystems.FileObject;
 import org.openide.util.Lookup;
+import org.openide.util.Utilities;
 import org.openide.util.lookup.Lookups;
 
 public class GenesisProject implements Project {
@@ -18,6 +38,8 @@ public class GenesisProject implements Project {
          "genesisBasedApplication.name";
    private static final String APPLICATION_DISPLAY_NAME_PROPERTY = 
          "genesisBasedApplication.prettyName";
+   private static final Icon PROJECT_ICON =  new ImageIcon(Utilities.loadImage(
+         Utils.ICON_PATH));
 
    private final class GenesisProjectInformation implements ProjectInformation {
       private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
@@ -33,7 +55,7 @@ public class GenesisProject implements Project {
       }
 
       public Icon getIcon() {
-         return null;
+         return PROJECT_ICON;
       }
 
       public Project getProject() {
--- plugins/netbeans/genesis-project-type/src/net/java/dev/genesis/plugins/netbeans/projecttype/ui/GenesisLogicalViewProvider.java
+++ plugins/netbeans/genesis-project-type/src/net/java/dev/genesis/plugins/netbeans/projecttype/ui/GenesisLogicalViewProvider.java
@@ -1,3 +1,21 @@
+/*
+ * The Genesis Project
+ * Copyright (C) 2006  Summa Technologies do Brasil Ltda.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package net.java.dev.genesis.plugins.netbeans.projecttype.ui;
 
 import java.awt.event.ActionEvent;
@@ -56,8 +74,7 @@ public class GenesisLogicalViewProvider implements LogicalViewProvider {
       public GenesisLogicalProviderRootNode() {
          super(new GenesisLogicalProviderChildren(), Lookups.singleton(project));
          setName(ProjectUtils.getInformation(project).getDisplayName());
-         setIconBaseWithExtension("net/java/dev/genesis/plugins/netbeans/" +
-               "projecttype/ui/resources/project_icon.gif");
+         setIconBaseWithExtension(Utils.ICON_PATH);
          setShortDescription(project.getProjectDirectory().getPath());
       }
 
