3587b5f8ad6271dbf7b584b0e5c7b0d40355a912
mister__m <misterm@gmail.com>
2006-04-04 21:27:30 +0000

    Missing copyright for 2006
    git-svn-id: https://svn.java.net/svn/genesis~svn/trunk@983 c226d7c4-892a-e4a7-9418-86fe0cfb12d3

--- genesis/modules/client/src/net/java/dev/genesis/ui/binding/AbstractBinder.java
+++ genesis/modules/client/src/net/java/dev/genesis/ui/binding/AbstractBinder.java
@@ -1,6 +1,6 @@
 /*
  * The Genesis Project
- * Copyright (C) 2005  Summa Technologies do Brasil Ltda.
+ * Copyright (C) 2005-2006  Summa Technologies do Brasil Ltda.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
