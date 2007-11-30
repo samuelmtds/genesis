ac81a6190a90863fdd6ac4b1f8def8a105bd10c6
Michel Graciano <michel.graciano@gmail.com>
2007-11-30 13:51:37 +0000

    Fixed license typo and removed unnecessary javadoc.
    git-svn-id: https://svn.java.net/svn/genesis~svn/trunk@1463 c226d7c4-892a-e4a7-9418-86fe0cfb12d3

--- plugins/netbeans/genesis-project-type/src/net/java/dev/genesis/plugins/netbeans/projecttype/ui/customizer/GenesisProjectProperties.java
+++ plugins/netbeans/genesis-project-type/src/net/java/dev/genesis/plugins/netbeans/projecttype/ui/customizer/GenesisProjectProperties.java
@@ -1,12 +1,21 @@
 /*
- * GenesisProjectProperties.java
+ * The Genesis Project
+ * Copyright (C) 2007  Summa Technologies do Brasil Ltda.
  *
- * Created on 25 de Agosto de 2007, 00:12
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 package net.java.dev.genesis.plugins.netbeans.projecttype.ui.customizer;
 
 
@@ -15,421 +24,201 @@ import net.java.dev.genesis.annotation.Form;
 import net.java.dev.genesis.plugins.netbeans.projecttype.GenesisProject;
 import net.java.dev.genesis.plugins.netbeans.projecttype.ui.customizer.annotation.Property;
 
-/**
- *
- * @author Michel Graciano
- */
 @Form
 public class GenesisProjectProperties {
-    
-    protected GenesisProject project;
-    
-    /** Creates a new instance of GenesisProjectProperties */
-    GenesisProjectProperties( GenesisProject project ) {
-        this.project = project;
-    }
-    
-    /**
-     * Mantém o valor da propriedade name.
-     */
-    private String name;
-    
-    /**
-     * "Getter" para a propriedade name.
-     * @return Valor para a propriedade name.
-     */
-    public String getName() {
-        return this.name;
-    }
-    
-    /**
-     * "Setter" para a propriedade name.
-     * @param name Novo valor para a propriedade name.
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-    /**
-     * Mantém o valor da propriedade prettyName.
-     */
-    private String prettyName;
-    
-    /**
-     * "Getter" para a propriedade prettyName.
-     * @return Valor para a propriedade prettyName.
-     */
-    @Property(value="genesisBasedApplication.prettyName")
-    public String getPrettyName() {
-        return this.prettyName;
-    }
-    
-    /**
-     * "Setter" para a propriedade prettyName.
-     * @param prettyName Novo valor para a propriedade prettyName.
-     */
-    public void setPrettyName(String prettyName) {
-        this.prettyName = prettyName;
-    }
-
-    /**
-     * Mantém o valor da propriedade mainClass.
-     */
-    private String mainClass;
-
-    /**
-     * "Getter" para a propriedade mainClass.
-     * @return Valor para a propriedade mainClass.
-     */
-    @Property("genesisBasedApplication.mainClass")
-    public String getMainClass() {
-        return this.mainClass;
-    }
-
-    /**
-     * "Setter" para a propriedade mainClass.
-     * @param mainClass Novo valor para a propriedade mainClass.
-     */
-    public void setMainClass(String mainClass) {
-        this.mainClass = mainClass;
-    }
-
-    /**
-     * Mantém o valor da propriedade localMode.
-     */
-    private boolean localMode;
-
-    /**
-     * "Getter" para a propriedade localMode.
-     * @return Valor para a propriedade localMode.
-     */
-    @Property("local.mode")
-    public boolean isLocalMode() {
-        return this.localMode;
-    }
-
-    /**
-     * "Setter" para a propriedade localMode.
-     * @param localMode Novo valor para a propriedade localMode.
-     */
-    public void setLocalMode(boolean localMode) {
-        this.localMode = localMode;
-    }
-
-    /**
-     * Mantém o valor da propriedade remoteMode.
-     */
-    private boolean remoteMode;
-
-    /**
-     * "Getter" para a propriedade remoteMode.
-     * @return Valor para a propriedade remoteMode.
-     */
-    @Property("remote.mode")
-    public boolean isRemoteMode() {
-        return this.remoteMode;
-    }
-
-    /**
-     * "Setter" para a propriedade remoteMode.
-     * @param remoteMode Novo valor para a propriedade remoteMode.
-     */
-    public void setRemoteMode(boolean remoteMode) {
-        this.remoteMode = remoteMode;
-    }
-
-    /**
-     * Mantém o valor da propriedade hibernateDialect.
-     */
-    private String hibernateDialect;
-
-    /**
-     * "Getter" para a propriedade hibernateDialect.
-     * @return Valor para a propriedade hibernateDialect.
-     */
-    @Property("hibernate.dialect")
-    @EnabledWhen("form.hibernateGenerateFiles")
-    public String getHibernateDialect() {
-        return this.hibernateDialect;
-    }
-
-    /**
-     * "Setter" para a propriedade hibernateDialect.
-     * @param hibernateDialect Novo valor para a propriedade hibernateDialect.
-     */
-    public void setHibernateDialect(String hibernateDialect) {
-        this.hibernateDialect = hibernateDialect;
-    }
-
-    /**
-     * Mantém o valor da propriedade validationGenerateFiles.
-     */
-    private boolean validationGenerateFiles;
-
-    /**
-     * "Getter" para a propriedade validationGenerateFiles.
-     * @return Valor para a propriedade validationGenerateFiles.
-     */
-    @Property("validation.generate.files")
-    public boolean isValidationGenerateFiles() {
-        return this.validationGenerateFiles;
-    }
-
-    /**
-     * "Setter" para a propriedade validationGenerateFiles.
-     * @param validationGenerateFiles Novo valor para a propriedade validationGenerateFiles.
-     */
-    public void setValidationGenerateFiles(boolean validationGenerateFiles) {
-        this.validationGenerateFiles = validationGenerateFiles;
-    }
-    
-    /**
-     * Mantém o valor da propriedade hibernateGenerateFiles.
-     */
-    private boolean hibernateGenerateFiles;
-
-    /**
-     * "Getter" para a propriedade hibernateGenerateFiles.
-     * @return Valor para a propriedade hibernateGenerateFiles.
-     */
-    @Property("hibernate.generate.files")
-    public boolean isHibernateGenerateFiles() {
-        return this.hibernateGenerateFiles;
-    }
-
-    /**
-     * "Setter" para a propriedade hibernateGenerateFiles.
-     * @param hibernateGenerateFiles Novo valor para a propriedade hibernateGenerateFiles.
-     */
-    public void setHibernateGenerateFiles(boolean hibernateGenerateFiles) {
-        this.hibernateGenerateFiles = hibernateGenerateFiles;
-    }
-
-    /**
-     * Mantém o valor da propriedade hibernateShowSql.
-     */
-    private boolean hibernateShowSql;
-
-    /**
-     * "Getter" para a propriedade hibernateShowSql.
-     * @return Valor para a propriedade hibernateShowSql.
-     */
-    @Property("hibernate.show.sql")
-    @EnabledWhen("form.hibernateGenerateFiles")
-    public boolean isHibernateShowSql() {
-        return this.hibernateShowSql;
-    }
-
-    /**
-     * "Setter" para a propriedade hibernateShowSql.
-     * @param hibernateShowSql Novo valor para a propriedade hibernateShowSql.
-     */
-    public void setHibernateShowSql(boolean hibernateShowSql) {
-        this.hibernateShowSql = hibernateShowSql;
-    }
-
-    /**
-     * Mantém o valor da propriedade hibernateVersion.
-     */
-    private String hibernateVersion;
-
-    /**
-     * "Getter" para a propriedade hibernateVersion.
-     * @return Valor para a propriedade hibernateVersion.
-     */
-    @Property("hibernate.version")
-    @EnabledWhen("form.hibernateGenerateFiles")
-    public String getHibernateVersion() {
-        return this.hibernateVersion;
-    }
-
-    /**
-     * "Setter" para a propriedade hibernateVersion.
-     * @param hibernateVersion Novo valor para a propriedade hibernateVersion.
-     */
-    public void setHibernateVersion(String hibernateVersion) {
-        this.hibernateVersion = hibernateVersion;
-    }
-
-    /**
-     * Mantém o valor da propriedade jdbcConnectionUrl.
-     */
-    private String jdbcConnectionUrl;
-
-    /**
-     * "Getter" para a propriedade jdbcConnectionUrl.
-     * @return Valor para a propriedade jdbcConnectionUrl.
-     */
-    @Property("jdbc.connection.url")
-    @EnabledWhen("form.hibernateGenerateFiles")
-    public String getJdbcConnectionUrl() {
-        return this.jdbcConnectionUrl;
-    }
-
-    /**
-     * "Setter" para a propriedade jdbcConnectionUrl.
-     * @param jdbcConnectionUrl Novo valor para a propriedade jdbcConnectionUrl.
-     */
-    public void setJdbcConnectionUrl(String jdbcConnectionUrl) {
-        this.jdbcConnectionUrl = jdbcConnectionUrl;
-    }
-
-    /**
-     * Mantém o valor da propriedade jdbcDriverClass.
-     */
-    private String jdbcDriverClass;
-
-    /**
-     * "Getter" para a propriedade jdbcDriverClass.
-     * @return Valor para a propriedade jdbcDriverClass.
-     */
-    @Property("jdbc.driver.class")
-    @EnabledWhen("form.hibernateGenerateFiles")
-    public String getJdbcDriverClass() {
-        return this.jdbcDriverClass;
-    }
-
-    /**
-     * "Setter" para a propriedade jdbcDriverClass.
-     * @param jdbcDriverClass Novo valor para a propriedade jdbcDriverClass.
-     */
-    public void setJdbcDriverClass(String jdbcDriverClass) {
-        this.jdbcDriverClass = jdbcDriverClass;
-    }
-
-    /**
-     * Mantém o valor da propriedade jdbcDriver.
-     */
-    private String jdbcDriver;
-
-    /**
-     * "Getter" para a propriedade jdbcDriver.
-     * @return Valor para a propriedade jdbcDriver.
-     */
-    @Property("jdbc.driver")
-    @EnabledWhen("form.hibernateGenerateFiles")
-    public String getJdbcDriver() {
-        return this.jdbcDriver;
-    }
-
-    /**
-     * "Setter" para a propriedade jdbcDriver.
-     * @param jdbcDriver Novo valor para a propriedade jdbcDriver.
-     */
-    public void setJdbcDriver(String jdbcDriver) {
-        this.jdbcDriver = jdbcDriver;
-    }
-
-    /**
-     * Mantém o valor da propriedade jdbcPassword.
-     */
-    private String jdbcPassword;
-
-    /**
-     * "Getter" para a propriedade jdbcPassword.
-     * @return Valor para a propriedade jdbcPassword.
-     */
-    @Property("jdbc.password")
-    @EnabledWhen("form.hibernateGenerateFiles")
-    public String getJdbcPassword() {
-        return this.jdbcPassword;
-    }
-
-    /**
-     * "Setter" para a propriedade jdbcPassword.
-     * @param jdbcPassword Novo valor para a propriedade jdbcPassword.
-     */
-    public void setJdbcPassword(String jdbcPassword) {
-        this.jdbcPassword = jdbcPassword;
-    }
-
-    /**
-     * Mantém o valor da propriedade jdbcUserName.
-     */
-    private String jdbcUserName;
-
-    /**
-     * "Getter" para a propriedade jdbcUserName.
-     * @return Valor para a propriedade jdbcUserName.
-     */
-    @Property("jdbc.username")
-    @EnabledWhen("form.hibernateGenerateFiles")
-    public String getJdbcUserName() {
-        return this.jdbcUserName;
-    }
-
-    /**
-     * "Setter" para a propriedade jdbcUserName.
-     * @param jdbcUserName Novo valor para a propriedade jdbcUserName.
-     */
-    public void setJdbcUserName(String jdbcUserName) {
-        this.jdbcUserName = jdbcUserName;
-    }
-
-    /**
-     * Mantém o valor da propriedade webstartPort.
-     */
-    private Long webstartPort;
-
-    /**
-     * "Getter" para a propriedade webstartPort.
-     * @return Valor para a propriedade webstartPort.
-     */
-    @Property("webstart.port")
-    public Long getWebstartPort() {
-        return this.webstartPort;
-    }
-
-    /**
-     * "Setter" para a propriedade webstartPort.
-     * @param webstartPort Novo valor para a propriedade webstartPort.
-     */
-    public void setWebstartPort(Long webstartPort) {
-        this.webstartPort = webstartPort;
-    }
-
-    /**
-     * Mantém o valor da propriedade webstartServer.
-     */
-    private String webstartServer;
-
-    /**
-     * "Getter" para a propriedade webstartServer.
-     * @return Valor para a propriedade webstartServer.
-     */
-    @Property("webstart.server")
-    public String getWebstartServer() {
-        return this.webstartServer;
-    }
-
-    /**
-     * "Setter" para a propriedade webstartServer.
-     * @param webstartServer Novo valor para a propriedade webstartServer.
-     */
-    public void setWebstartServer(String webstartServer) {
-        this.webstartServer = webstartServer;
-    }
-
-    /**
-     * Mantém o valor da propriedade buildDir.
-     */
-    private String buildDir;
-
-    /**
-     * "Getter" para a propriedade buildDir.
-     * @return Valor para a propriedade buildDir.
-     */
-    @Property("build.dir")
-    public String getBuildDir() {
-        return this.buildDir;
-    }
-
-    /**
-     * "Setter" para a propriedade buildDir.
-     * @param buildDir Novo valor para a propriedade buildDir.
-     */
-    public void setBuildDir(String buildDir) {
-        this.buildDir = buildDir;
-    }
-    
+   
+   protected GenesisProject project;
+   
+   private String name;
+   private String prettyName;
+   private String mainClass;
+   private boolean localMode;
+   private boolean remoteMode;
+   private String hibernateDialect;
+   private boolean validationGenerateFiles;
+   private boolean hibernateGenerateFiles;
+   private boolean hibernateShowSql;
+   private String hibernateVersion;
+   private String jdbcConnectionUrl;
+   private String jdbcDriverClass;
+   private String jdbcDriver;
+   private String jdbcPassword;
+   private String jdbcUserName;
+   private Long webstartPort;
+   private String webstartServer;
+   private String buildDir;
+   
+   GenesisProjectProperties( GenesisProject project ) {
+      this.project = project;
+   }
+   
+   public String getName() {
+      return this.name;
+   }
+   
+   public void setName(String name) {
+      this.name = name;
+   }
+   
+   @Property(value="genesisBasedApplication.prettyName")
+   public String getPrettyName() {
+      return this.prettyName;
+   }
+   
+   public void setPrettyName(String prettyName) {
+      this.prettyName = prettyName;
+   }
+   
+   @Property("genesisBasedApplication.mainClass")
+   public String getMainClass() {
+      return this.mainClass;
+   }
+   
+   public void setMainClass(String mainClass) {
+      this.mainClass = mainClass;
+   }
+   
+   @Property("local.mode")
+   public boolean isLocalMode() {
+      return this.localMode;
+   }
+   
+   public void setLocalMode(boolean localMode) {
+      this.localMode = localMode;
+   }
+   
+   @Property("remote.mode")
+   public boolean isRemoteMode() {
+      return this.remoteMode;
+   }
+   
+   public void setRemoteMode(boolean remoteMode) {
+      this.remoteMode = remoteMode;
+   }
+   
+   @Property("hibernate.dialect")
+   @EnabledWhen("form.hibernateGenerateFiles")
+   public String getHibernateDialect() {
+      return this.hibernateDialect;
+   }
+   
+   public void setHibernateDialect(String hibernateDialect) {
+      this.hibernateDialect = hibernateDialect;
+   }
+   
+   @Property("validation.generate.files")
+   public boolean isValidationGenerateFiles() {
+      return this.validationGenerateFiles;
+   }
+   
+   public void setValidationGenerateFiles(boolean validationGenerateFiles) {
+      this.validationGenerateFiles = validationGenerateFiles;
+   }
+   
+   @Property("hibernate.generate.files")
+   public boolean isHibernateGenerateFiles() {
+      return this.hibernateGenerateFiles;
+   }
+   
+   public void setHibernateGenerateFiles(boolean hibernateGenerateFiles) {
+      this.hibernateGenerateFiles = hibernateGenerateFiles;
+   }
+   
+   @Property("hibernate.show.sql")
+   @EnabledWhen("form.hibernateGenerateFiles")
+   public boolean isHibernateShowSql() {
+      return this.hibernateShowSql;
+   }
+   
+   public void setHibernateShowSql(boolean hibernateShowSql) {
+      this.hibernateShowSql = hibernateShowSql;
+   }
+   
+   @Property("hibernate.version")
+   @EnabledWhen("form.hibernateGenerateFiles")
+   public String getHibernateVersion() {
+      return this.hibernateVersion;
+   }
+   
+   public void setHibernateVersion(String hibernateVersion) {
+      this.hibernateVersion = hibernateVersion;
+   }
+   
+   @Property("jdbc.connection.url")
+   @EnabledWhen("form.hibernateGenerateFiles")
+   public String getJdbcConnectionUrl() {
+      return this.jdbcConnectionUrl;
+   }
+   
+   public void setJdbcConnectionUrl(String jdbcConnectionUrl) {
+      this.jdbcConnectionUrl = jdbcConnectionUrl;
+   }
+   
+   @Property("jdbc.driver.class")
+   @EnabledWhen("form.hibernateGenerateFiles")
+   public String getJdbcDriverClass() {
+      return this.jdbcDriverClass;
+   }
+   
+   public void setJdbcDriverClass(String jdbcDriverClass) {
+      this.jdbcDriverClass = jdbcDriverClass;
+   }
+   
+   @Property("jdbc.driver")
+   @EnabledWhen("form.hibernateGenerateFiles")
+   public String getJdbcDriver() {
+      return this.jdbcDriver;
+   }
+   
+   public void setJdbcDriver(String jdbcDriver) {
+      this.jdbcDriver = jdbcDriver;
+   }
+   
+   @Property("jdbc.password")
+   @EnabledWhen("form.hibernateGenerateFiles")
+   public String getJdbcPassword() {
+      return this.jdbcPassword;
+   }
+   
+   public void setJdbcPassword(String jdbcPassword) {
+      this.jdbcPassword = jdbcPassword;
+   }
+   
+   @Property("jdbc.username")
+   @EnabledWhen("form.hibernateGenerateFiles")
+   public String getJdbcUserName() {
+      return this.jdbcUserName;
+   }
+   
+   public void setJdbcUserName(String jdbcUserName) {
+      this.jdbcUserName = jdbcUserName;
+   }
+   
+   @Property("webstart.port")
+   public Long getWebstartPort() {
+      return this.webstartPort;
+   }
+   
+   public void setWebstartPort(Long webstartPort) {
+      this.webstartPort = webstartPort;
+   }
+   
+   @Property("webstart.server")
+   public String getWebstartServer() {
+      return this.webstartServer;
+   }
+   
+   public void setWebstartServer(String webstartServer) {
+      this.webstartServer = webstartServer;
+   }
+   
+   @Property("build.dir")
+   public String getBuildDir() {
+      return this.buildDir;
+   }
+   
+   public void setBuildDir(String buildDir) {
+      this.buildDir = buildDir;
+   }
+   
 }
