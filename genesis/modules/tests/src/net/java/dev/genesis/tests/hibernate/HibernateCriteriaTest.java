228a2980b4d0fcd3d3fe9b13825f6ba31db86c00
mister__m <misterm@gmail.com>
2008-06-10 15:10:48 +0000

    IllegalArgumentException is thrown if HibernateCriteria hasn't been prepared for execution (issue # 496)
    git-svn-id: https://svn.java.net/svn/genesis~svn/trunk@1567 c226d7c4-892a-e4a7-9418-86fe0cfb12d3

--- genesis/modules/tests/src/net/java/dev/genesis/tests/hibernate/HibernateCriteriaTest.java
+++ genesis/modules/tests/src/net/java/dev/genesis/tests/hibernate/HibernateCriteriaTest.java
@@ -1,6 +1,6 @@
 /*
  * The Genesis Project
- * Copyright (C) 2004-2005  Summa Technologies do Brasil Ltda.
+ * Copyright (C) 2004-2008  Summa Technologies do Brasil Ltda.
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
