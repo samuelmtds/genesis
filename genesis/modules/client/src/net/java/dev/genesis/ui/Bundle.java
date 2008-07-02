f9a77e65112b51d3dcb45f00f21f27dd4337ebb2
Michel Graciano <michel.graciano@gmail.com>
2008-07-02 17:10:48 +0000

    Initial support for genesis messages internationalization (Issue # 478)
    git-svn-id: https://svn.java.net/svn/genesis~svn/trunk@1571 c226d7c4-892a-e4a7-9418-86fe0cfb12d3

---
+++ genesis/modules/client/src/net/java/dev/genesis/ui/Bundle.java
@@ -0,0 +1,78 @@
+/*
+ * The Genesis Project
+ * Copyright (C) 2008 Summa Technologies do Brasil Ltda.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package net.java.dev.genesis.ui;
+
+import java.util.Locale;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class Bundle {
+   public static String getMessage(Class clazz, String key) throws
+         MissingResourceException {
+      return getBundle(clazz).getString(key);
+   }
+
+   public static String getMessage(Class clazz, String key, Object param1)
+         throws MissingResourceException {
+      return getMessage(clazz, key, new Object[] {param1});
+   }
+
+   public static String getMessage(Class clazz, String key, Object param1,
+         Object param2) throws MissingResourceException {
+      return getMessage(clazz, key, new Object[] {param1, param2});
+   }
+
+   public static String getMessage(Class clazz, String key, Object[] arr)
+         throws MissingResourceException {
+      return java.text.MessageFormat.format(getMessage(clazz, key), arr);
+   }
+
+   public static ResourceBundle getBundle(Class clazz) throws
+         MissingResourceException {
+      String name = findName(clazz);
+      return getBundle(name, Locale.getDefault(), clazz.getClassLoader());
+   }
+
+   /** Finds package name for given class */
+   private static String findName(Class clazz) {
+      String pref = clazz.getName();
+      int last = pref.lastIndexOf('.');
+
+      if (last >= 0) {
+         pref = pref.substring(0, last + 1);
+
+         return pref + "Bundle"; // NOI18N
+      } else {
+         // base package, search for bundle
+         return "Bundle"; // NOI18N
+      }
+   }
+
+   public static final ResourceBundle getBundle(String baseName, Locale locale,
+         ClassLoader loader) throws MissingResourceException {
+      ResourceBundle b = ResourceBundle.getBundle(baseName, locale, loader);
+
+      if (b != null) {
+         return b;
+      } else {
+         throw new MissingResourceException("No such bundle " + baseName, // NOI18N
+               baseName, null);
+      }
+   }
+}
---
+++ genesis/modules/client/test-src/net/java/dev/genesis/ui/BundleTest.java
@@ -0,0 +1,83 @@
+/*
+ * The Genesis Project
+ * Copyright (C) 2008 Summa Technologies do Brasil Ltda.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package net.java.dev.genesis.ui;
+
+import java.util.Locale;
+import java.util.MissingResourceException;
+import net.java.dev.genesis.GenesisTestCase;
+
+public class BundleTest extends GenesisTestCase {
+   public BundleTest(String testName) {
+      super(testName);
+   }
+
+   protected void setUp() throws Exception {
+   }
+
+   protected void tearDown() throws Exception {
+   }
+
+   public void testGetMessage() {
+      final Locale defaultLocale = Locale.getDefault();
+      Locale.setDefault(Locale.ENGLISH);
+      try {
+         Bundle.getMessage(BundleTest.class, "falt");
+         fail("A MissingResourceException should be thrown if a invalid " +
+               "resource name is informed");
+      } catch (MissingResourceException mre) {
+      }
+      assertEquals("Test", Bundle.getMessage(BundleTest.class, "test"));
+      assertEquals("Default", Bundle.getMessage(BundleTest.class, "default"));
+      assertEquals("Message from genesis", Bundle.getMessage(BundleTest.class,
+            "message_0_1", new Object[] {"Message", "genesis"}));
+      assertEquals("Message from genesis", Bundle.getMessage(BundleTest.class,
+            "message_0_1", "Message", "genesis"));
+      assertEquals("Message from genesis", Bundle.getMessage(BundleTest.class,
+            "message_0", "genesis"));
+
+
+      Locale pt_BR = new Locale("pt", "BR");
+      Locale.setDefault(pt_BR);
+      assertEquals("Teste", Bundle.getMessage(BundleTest.class, "test"));
+      assertEquals("Default", Bundle.getMessage(BundleTest.class, "default"));
+      assertEquals("Mensagem do genesis", Bundle.getMessage(BundleTest.class,
+            "message_0_1", new Object[] {"Mensagem", "genesis"}));
+      assertEquals("Mensagem do genesis", Bundle.getMessage(BundleTest.class,
+            "message_0_1", "Mensagem", "genesis"));
+      assertEquals("Mensagem do genesis", Bundle.getMessage(BundleTest.class,
+            "message_0", "genesis"));
+
+      Locale.setDefault(defaultLocale);
+   }
+
+   public void testGetBundle() {
+      try {
+         Bundle.getBundle("Error", Locale.getDefault(), Thread.currentThread().
+               getContextClassLoader());
+         fail("A MissingResourceException should be thrown if a invalid " +
+               "bundle name is informed");
+      } catch (MissingResourceException mre) {
+      }
+
+      Bundle.getBundle(BundleTest.class.getPackage().getName() + ".Bundle",
+            Locale.getDefault(), Thread.currentThread().getContextClassLoader());
+
+      Bundle.getBundle(BundleTest.class);
+   }
+}
